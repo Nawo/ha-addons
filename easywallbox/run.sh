38606a4f670ad483ccebeb5ae8c11c65726f9b78
Nawo <romug1337@gmail.com>
2026-01-05 19:30:12 +0100

    change some things

--- easywallbox/run.sh
+++ easywallbox/run.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-set -e
+echo "--- Uruchamianie EasyWallbox Controller ---"
 
-echo "Uruchamianie EasyWallbox Controller..."
+CONFIG_PATH=/data/options.json
 
-export MQTT_HOST=$(jq --raw-output '.mqtt_host // empty' /data/options.json)
-export MQTT_PORT=$(jq --raw-output '.mqtt_port // 1883' /data/options.json)
-export MQTT_USERNAME=$(jq --raw-output '.mqtt_user // empty' /data/options.json)
-export MQTT_PASSWORD=$(jq --raw-output '.mqtt_password // empty' /data/options.json)
-export WALLBOX_ADDRESS=$(jq --raw-output '.wallbox_mac // empty' /data/options.json)
-export WALLBOX_PIN=$(jq --raw-output '.wallbox_pin // "0000"' /data/options.json)
+export MQTT_HOST=$(jq --raw-output '.mqtt_host' $CONFIG_PATH)
+export MQTT_PORT=$(jq --raw-output '.mqtt_port' $CONFIG_PATH)
+export MQTT_USERNAME=$(jq --raw-output '.mqtt_user' $CONFIG_PATH)
+export MQTT_PASSWORD=$(jq --raw-output '.mqtt_password' $CONFIG_PATH)
+export WALLBOX_ADDRESS=$(jq --raw-output '.wallbox_mac' $CONFIG_PATH)
+export WALLBOX_PIN=$(jq --raw-output '.wallbox_pin' $CONFIG_PATH)
 
-echo "Konfiguracja załadowana."
+echo "Konfiguracja załadowana:"
 echo "Wallbox address: $WALLBOX_ADDRESS"
 echo "Broker MQTT: $MQTT_HOST"
 
-echo "Pobieranie najnowszej wersji kodu..."
+echo "Przechodzę do katalogu aplikacji..."
 cd /app
 
 git pull
 
-echo "Startuje EasyWallbox..."
+echo "Uruchamiam skrypt Python..."
 python3 easywallbox.py
